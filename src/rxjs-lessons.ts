007115803723b63808b466cd6c7a01be1b220a6f
TaOleks <tahtay.aleksey@gmail.com>
2023-07-28 23:14:41 +0200

    Next, Error, Complete

--- src/rxjs-lessons.ts
+++ src/rxjs-lessons.ts
@@ -1,20 +1,33 @@
 import { Observable } from "rxjs";
 
 const search$ = new Observable(observer =>{
-  console.log('start in observable')
-  observer.next(1);
-  observer.next(2);
-  observer.next(3);
-  console.log('End in observable')
+
+
+  const search = document.getElementById('search')
+
+   if(!search){
+    observer.error('Element does not exists on the page')
+    return;
+   }
+  search?.addEventListener('input',event =>{
+
+    observer.next(event)
+    observer.complete()
+  })
+
+
 });
-console.log('Start subscribe')
 
-search$.subscribe(value =>{
-  console.log(value)
+
+search$.subscribe({
+  next:value=>{
+  console.log(value);
+  },
+  error:err => console.log(err),
+  complete: ()=> {
+    console.log('Event end')
+  }
 })
-console.log('End subscribe')
 
 
-search$.subscribe(value =>{
-  console.log(value)
-})
+
