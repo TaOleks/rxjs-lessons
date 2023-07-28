9571b118728177eb745be92c1699e3561c8accde
TaOleks <tahtay.aleksey@gmail.com>
2023-07-28 16:00:04 +0200

    create observable and subscribe

---
+++ src/rxjs-lessons.ts
@@ -0,0 +1,20 @@
+import { Observable } from "rxjs";
+
+const search$ = new Observable(observer =>{
+  console.log('start in observable')
+  observer.next(1);
+  observer.next(2);
+  observer.next(3);
+  console.log('End in observable')
+});
+console.log('Start subscribe')
+
+search$.subscribe(value =>{
+  console.log(value)
+})
+console.log('End subscribe')
+
+
+search$.subscribe(value =>{
+  console.log(value)
+})
